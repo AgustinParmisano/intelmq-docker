d0f2da6cdeac967537d8ef0fa5cd424d9800c6af
Agustin Parmisano <agustinparmisano@gmail.com>
2023-10-31 20:10:57 -0300

    Firsts Testings in IntelMQ: adding new parser bots

--- thintel_bots/bots/parsers/abusech/parser_feodotracker2023.py
+++ thintel_bots/bots/parsers/abusech/parser_feodotracker2023.py
@@ -12,38 +12,17 @@ from intelmq.lib.bot import ParserBot
 class AbusechFeodoTrackerParserBot2023(ParserBot):
     """Parse the Abuse.ch Feodo Tracker feed """
 
-    def parse(self, report):
-        raw_report = report.strip()
-
-        if not len(raw_report):  # We depend on first line = date
-            return
-
-        first_row = raw_report.splitlines()[0]
-        time_match = re.search(r'Last updated: (\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} UTC)', first_row)
-        
-        if time_match:
-            time_str = time_match.group(1)
-            self.last_updated = dateutil.parser.parse(time_str).isoformat()
-        else:
-            return
-
-        for row in raw_report.splitlines():
-            if not row.startswith("#"):
-                yield row.strip()
-
-    def parse_line(self, val, report):
-        if not val or val.startswith('#'):
-            return
-
-        event = self.new_event(report)
-
-        if not event.add('source.ip', val, raise_failure=False):
-            event.add('source.network', val)
-
-        event.add('time.source', self.last_updated)
-        event.add('classification.type', 'blacklist')
-        event.add('raw', val)
-
-        yield event
+    def parse_line(self, line, report):
+        line = line.strip()
+        if line.startswith('# Last updated:'):
+            self._lastupdated = line[16:36]
+        elif re.match(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', line):
+            event = self.new_event()
+            if self._lastupdated:
+                event.add('time.source', dateutil.parser.parse(self._lastupdated).isoformat())
+            event.add('classification.type', 'malware-distribution')
+            event.add('source.ip', line)
+            
+            yield event
 
 BOT = AbusechFeodoTrackerParserBot2023
